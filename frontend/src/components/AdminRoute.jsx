009f77e9cd126e31c294ce3b5e73bea605cd3684
shaflynurrasyid <mshaflyn24@gmail.com>
2023-12-13 16:25:26 +0700

    nambahin console log

--- frontend/src/components/AdminRoute.jsx
+++ frontend/src/components/AdminRoute.jsx
@@ -3,6 +3,8 @@ import { useSelector } from 'react-redux';
 
 const AdminRoute = () => {
   const { userInfo } = useSelector((state) => state.auth);
+  console.log("Print lala " + userInfo)
+  console.log("Print yeye " + userInfo.isAdmin)
   return userInfo && userInfo.isAdmin ? (
     <Outlet />
   ) : (
